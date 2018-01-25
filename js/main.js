7be51d024538fad1030dd799a32327e51d26c274
wangchen <35281799+chen1code@users.noreply.github.com>
2018-01-25 20:47:43 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -81,9 +81,9 @@
 	}
 	function init_callbak(time_i)
 	{
-		var dm_value_arr = ["你怎么这样","2","3","4"];
-		var dm_size_arr = ["12px","15px","14px","12px"];
-		var dm_color_arr = ["000","000","ccc","ddd"]		
+		var dm_value_arr = ["日期记不得了吗","笨蛋","好好想想啦","一周年哎","往回推365天不就知道啦","难道要我直接跟你说啊","要是不记得的话就太过分啦","还没想起来么","是17年啦","2017年1月26号","写对了么","下次不能忘咯","忘了是要打屁屁的哟","一定要记住啊","爱你哟"."muma~"];
+		var dm_size_arr = ["12px","15px","14px","12px","12px","15px","14px","12px","12px","15px","14px","12px"];
+		var dm_color_arr = ["000","000","ccc","ddd","cb2431","0366d6","34d058","F75000","FF8000","CE0000","9D9D9D","FFF4C1","ddd","cb2431","0366d6","34d058"]		
 		var dm_value = dm_value_arr[time_i],dm_size = dm_size_arr[time_i],dm_color = dm_color_arr[time_i];
 		dm_value_length = dm_value_arr.length;
 		mu(dm_value,dm_size,dm_color);
@@ -140,22 +140,50 @@
 		//$(this).slideUp()
 	})
 	var count_error = 0;
-	$(".btn_confirm").click(function(){		
+	$(".btn_confirm").click(function(){	
+		event.stopPropagation();
 		var index = $(this).parent().parent().index() + 1;
 		var check_value = $(".answer-choice input:radio[name='answer"+ index + "']:checked").val();
-		if(check_value == "true"){
+		if(count_error<3){
+			if(check_value == "true"){
 			$(this).parent().parent().next("li").find(".bg-mask").animate({top:"-200"},2000);
+			$(this).hide().siblings("button").hide();
+			$(this).siblings(".right_result").show();
 		}else{
 			count_error += 1; 
+			if(count_error>=4){
+				count_error=3
+			}
 			if(count_error == 1){
 				$(".life-num i").eq(2).removeClass("fa-heart").addClass("fa-heart-o")
 			}else if(count_error == 2){
 				$(".life-num i").eq(1).removeClass("fa-heart").addClass("fa-heart-o")
 			}else if(count_error == 3){
 				$(".life-num i").eq(0).removeClass("fa-heart").addClass("fa-heart-o");
-				$(".mask").show()
+				$(".mask").show();
+				$(".mask-panel").show()
 			}
 		}
-		count_error = count_error;
+		}else{
+			$(".mask").show();
+			$(".mask-panel").show()
+		}
+		
+		
 	})
-	
+	$(".mask").click(function(){
+		$(".mask").hide();
+		$(".mask-panel").hide()
+	})
+	$(".relife-btn").click(function(){
+		var coin_num = $(".relife_num span").html();
+		if(coin_num>=5){
+			$(".relife_num span").html(coin_num-5);
+			$(".life-num i").removeClass("fa-heart-o").addClass("fa-heart");
+			count_error = 0;
+			$(".mask").hide();
+			$(".mask-panel").hide();
+		}else{
+			alert("先充值在继续哟~~")
+		}
+	})
